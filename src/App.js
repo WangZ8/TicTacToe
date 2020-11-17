98c770bf3cd6926a0eebf9663059029b94db3775
WangZ8 <zhope8@hotmail.com>
2020-11-17 10:38:55 +0100

    Use function component instead class

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './Game.css';
 import Game from './Game.js';
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Tic-Tac-Toe Game</h2>
-        </div>
-        <p className="App-intro">
-          <Game></Game>
-        </p>
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Tic-Tac-Toe Game</h2>
       </div>
-    );
-  }
+      <p className="App-intro">
+        <Game></Game>
+      </p>
+    </div>
+  );
 }
 
 export default App;
--- src/Board.js
+++ src/Board.js
@@ -2,31 +2,25 @@ import React from 'react';
 
 import Square from './Square';
 
-class Board extends React.Component {
-  
-  renderSquare(i) {
-    return <Square value={this.props.squares[i]}/>;
-  }
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+function Board() {
+  return (
+    <div>
+      <div className="board-row">
+        {Square(0)}
+        {Square(1)}
+        {Square(2)}
       </div>
-    );
-  }
+      <div className="board-row">
+        {Square(3)}
+        {Square(4)}
+        {Square(5)}
+      </div>
+      <div className="board-row">
+        {Square(6)}
+        {Square(7)}
+        {Square(8)}
+      </div>
+    </div>
+  );
 }
 export default Board;
